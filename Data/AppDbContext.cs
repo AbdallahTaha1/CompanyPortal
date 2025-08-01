219e877fefb971ae7d6cd6760a7d482ace5a44dc
Abdullah Taha <engbody007@gmail.com>
2025-08-01 19:19:53 +0300

    feat: add EF Core Fluent API configuration for Company entity

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,6 +9,14 @@ namespace CompanyPortal.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Company> Companies { get; set; }
     }
---
+++ Data/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using CompanyPortal.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CompanyPortal.Data.Configurations
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.ArabicName)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(c => c.EnglishName)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(c => c.PhoneNumber)
+                .HasMaxLength(20);
+
+            builder.Property(c => c.WebsiteUrl)
+                .HasMaxLength(300);
+
+            builder.Property(c => c.LogoUrl)
+                .HasMaxLength(300);
+
+            // Relationships
+            builder.HasOne(c => c.User)
+                   .WithOne(u => u.Company)
+                   .HasForeignKey<Company>(c => c.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
